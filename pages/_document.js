c571dc3f4a66f48ba1e77b8369de35e1e8e84bc5
Rahul Kumar <rahul.kumar@softage.net>
2024-04-30 16:48:36 +0530

    admin

--- pages/_document.js
+++ pages/_document.js
@@ -158,6 +158,16 @@ class MyDocument extends Document {
             }}
           />
 
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "m4nzlpki8i");`,
+            }}
+          />
+
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
