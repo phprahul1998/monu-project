db78c753ea751fecafc5e5c56becea5193a8f8ed
Rahul Kumar <rahul.kumar@softage.net>
2024-04-30 15:35:15 +0530

    admin1

--- pages/data.js
+++ pages/data.js
@@ -18,7 +18,6 @@ export default function data({ translations }) {
       console.error("Error saving translations:", error);
     }
   };
-
   const getjsonData = async () => {
     try {
       const response = await axios.post("api/get_translations");
