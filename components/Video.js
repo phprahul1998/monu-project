2eea5094e52836fb81101aef4459bbbfc3cab3ea
spacerahul.in.net <39645937+phprahul1998@users.noreply.github.com>
2024-05-18 13:34:02 +0530

    change class to className

--- components/Video.js
+++ components/Video.js
@@ -20,7 +20,7 @@ export default function Video() {
 
  
   return (
-    <main  class="project-area area-padding imagegallery">
+    <main  className="project-area area-padding imagegallery">
         <div className="container">
         <div className="row">
           <div className="col-md-12 col-lg-12 col-xl-12 col-sm-12 col-xs-12 vc" >
